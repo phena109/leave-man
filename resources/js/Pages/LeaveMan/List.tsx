0d0eb431e2f69236dbb99029e6f67fdf1ad87f38
Douglas <phena109@gmail.com>
2023-05-13 23:56:17 +1000

    Do tsx instead of jsx. Placeholder code for the list

---
+++ resources/js/Pages/LeaveMan/List.tsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/react";
+
+export default function List() {
+    return (
+        <>
+            <Head title="Leave Management List"></Head>
+            <div>
+                <h1>Leave Management List</h1>
+            </div>
+        </>
+    )
+}
