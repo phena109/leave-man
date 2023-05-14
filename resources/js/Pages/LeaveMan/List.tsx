7dddc5a38ad0f579c9fdfdee5b0d81eaf2a45810
Douglas <phena109@gmail.com>
2023-05-14 23:36:38 +1000

    WIP

--- resources/js/Pages/LeaveMan/List.tsx
+++ resources/js/Pages/LeaveMan/List.tsx
@@ -4,8 +4,16 @@ export default function List() {
     return (
         <>
             <Head title="Leave Management List"></Head>
-            <div>
+            <div className="bg-gray-100 h-screen">
                 <h1>Leave Management List</h1>
+                <div className="bg-white p-4 m-4 rounded-lg">
+                    <ul>
+                        <li>Item 1</li>
+                        <li>Item 2</li>
+                        <li>Item 3</li>
+                    </ul>
+                </div>
+                <br/>
             </div>
         </>
     )
